b0fd4e72ec0e56257991487700a066cf56fa41db
ormeno <rodrigo.ormeno.jabonero@gmail.com>
2022-02-11 15:25:08 +0100

    commit 11-2

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.engine('.hbs',exphbs({
     defaultLayout: 'main',
+    partialsDir: path.join(app.get('views'), 'partials'),
     layoutsDir: path.join(app.get('views'), 'layouts'),
     extname: '.hbs'
 }))
@@ -21,10 +22,30 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+fakeApi = () => {
+  return [
+    {
+      name: 'Katarina',
+      lane: 'midlaner'
+    },
+    {
+      name: 'Jayce',
+      lane: 'toplaner'
+    }, 
+    {
+      name: 'Azir',
+      lane: 'midlaner'
+    }
+  ];
+}
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {layout: 'main', suggestedChamps: fakeApi(), listExists: true});
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 app.listen(app.get('port'), () => {
   console.log('Servidor en puerto ', port);
